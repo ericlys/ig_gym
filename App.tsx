aa7f0fd22aaff4d20a305cfbb63b8b5c2b38e95e
Ericlys Moreira <ericlysm@gmail.com>
2023-03-06 17:23:33 -0300

    chore: default theme

--- App.tsx
+++ App.tsx
@@ -1,9 +1,12 @@
-import { StatusBar, Text, View } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import {
   useFonts,
   Roboto_400Regular,
   Roboto_700Bold
 } from '@expo-google-fonts/roboto'
+import { NativeBaseProvider } from 'native-base';
+import { Loading } from '@components/Loading';
+import { THEME } from './src/theme';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -12,13 +15,13 @@ export default function App() {
   })
 
   return (
-    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#202024'}} >
+    <NativeBaseProvider theme={THEME}>
       <StatusBar
         barStyle="light-content"
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Text>Hello World!</Text> : <View/>}
-    </View>
+     {fontsLoaded ? <View/> : <Loading/>}
+    </NativeBaseProvider>
   );
 }
