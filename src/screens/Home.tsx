7e892f30c6cb4d3647fa76eec0d21ca15813db74
Ericlys Moreira <ericlysm@gmail.com>
2023-03-08 17:39:48 -0300

    refactor: group menu isActive

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -22,7 +22,7 @@ export function Home() {
         renderItem={({item}) => (
           <Group 
             name={item} 
-            isActive={groupSelected === item }
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase() }
             onPress={() => setGroupSelected(item)}
           />
         )}
