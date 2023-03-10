e11b1ee364988c1227d5a35b068c30a856a45f18
Ericlys Moreira <ericlysm@gmail.com>
2023-03-10 16:13:59 -0300

    chore: looking for image from ghallery

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
-import { Center, Heading, ScrollView, Skeleton, Text, VStack } from "native-base";
+import { Center, Heading, ScrollView, Skeleton, Text, useToast, VStack } from "native-base";
 import * as ImagePicker from "expo-image-picker";
+import * as FileSystem from 'expo-file-system';
 
 import { ScreenHeader } from "@components/ScreenHeader";
 import { UserPhoto } from "@components/UserPhoto";
@@ -12,9 +13,45 @@ const PHOTO_SIZE = 33;
 
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
+  const [userPhoto, setUserPhoto] = useState('https://github.com/ericlys.png');
+
+  const toast = useToast()
 
   async function handleUaserPhotoSelect(){
-    await ImagePicker.launchImageLibraryAsync()
+    setPhotoIsLoading(true);
+
+    try {
+      const photoSelected = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        quality: 1,
+        aspect: [4,4],
+        allowsEditing: true,
+        base64: true
+      })
+      
+      if(photoSelected.canceled) {
+        return
+      }
+
+      if(photoSelected.assets[0].uri) {
+        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri)
+        
+        if(photoInfo.exists && (photoInfo.size / 1024 / 1024) > 5){
+          return toast.show({
+            title: "Essa imagem é muito grande. Escolha uma de até 5MB.",
+            placement: 'top',
+            bgColor: 'red.500'
+          })          
+        }
+
+        setUserPhoto(photoSelected.assets[0].uri)        
+      }
+      
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setPhotoIsLoading(false)
+    }
   }
 
   return(
@@ -32,7 +69,7 @@ export function Profile() {
             endColor="gray.400"
           >
             <UserPhoto 
-              source={{uri: "https://github.com/ericlys.png"}} 
+              source={{uri: userPhoto}} 
               alt="Foto do usuário"
               size={PHOTO_SIZE} 
            />
