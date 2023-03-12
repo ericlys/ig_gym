80be126693406b9876a28a7445fb698300aee041
Ericlys Moreira <ericlysm@gmail.com>
2023-03-11 22:49:40 -0300

    chore: creating user

--- mobile/src/screens/SignUp.tsx
+++ mobile/src/screens/SignUp.tsx
@@ -33,8 +33,19 @@ export function SignUp() {
     navigation.goBack();
   }
 
-  function handleSignUp(data: FormDataProps) {
-    console.log(data);
+  async function handleSignUp({ name, email, password}: FormDataProps) {
+    const response = await fetch('http://172.21.0.1:3333/users', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({name, email, password})
+    })
+    
+    const data = await response.json()
+    console.log(data)
+
     reset();
     navigation.goBack();
   }
