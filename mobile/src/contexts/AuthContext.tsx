437c6c833f1b9aedf2855d5eb4fd3c39875a1ad1
Ericlys Moreira <ericlysm@gmail.com>
2023-03-13 22:11:53 -0300

    feat: signIn in back-end router

---
+++ mobile/src/contexts/AuthContext.tsx
@@ -0,0 +1,65 @@
+import { UserDTO } from "@dtos/UserDTO";
+import { api } from "@services/api";
+import { storageUserGet, storageUserSave } from "@storage/storageUser";
+import { createContext, ReactNode, useEffect, useState } from "react";
+
+export type AuthContextDataProps = {
+  user: UserDTO;
+  signIn: (email: string, password: string) => Promise<void>;
+  isLoadingUserStorageData: boolean;
+}
+
+type AuthContextProviderProps = {
+  children: ReactNode;
+}
+
+export const AuthContext = createContext({} as AuthContextDataProps);
+
+export function AuthContextProvider({ children }: AuthContextProviderProps) {
+  const [ user, setUser ] = useState({} as UserDTO);
+  const [ isLoadingUserStorageData, setIsLoadingUserStorageData] = useState(true);
+
+  async function signIn(email: string, password: string) { 
+
+    try {
+      const { data } = await api.post('/sessions', { email, password })
+      
+      if (data.user) {
+        setUser(data.user);
+        storageUserSave(data.user);
+      }
+    } catch (error) {
+      throw error;
+    }
+    
+  }
+
+  async function loadUserData(){
+    try {
+      const userLogged = await storageUserGet();
+      
+      if (userLogged) {
+        setUser(userLogged);
+      }
+    } catch (err) {
+      throw err;
+    } finally {
+      setIsLoadingUserStorageData(false);
+    }
+  }
+
+  useEffect(() => {
+    loadUserData()
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ 
+      user, 
+      signIn,
+      isLoadingUserStorageData      
+      }}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
--- mobile/src/screens/SignIn.tsx
+++ mobile/src/screens/SignIn.tsx
@@ -1,19 +1,61 @@
 import { useNavigation } from "@react-navigation/native";
-import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import { Center, Heading, Image, ScrollView, Text, useToast, VStack } from "native-base";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 import LogoSvg from '@assets/logo.svg'
 import BackgroundImg from '@assets/background.png'
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
+
 import { AuthNavigatorRoutesProps } from "@routes/auth.routes";
+import { useForm, Controller } from "react-hook-form";
+import { useAuth } from "@hooks/useAuth";
+import { AppError } from "@utils/AppError";
+import { useState } from "react";
+
+type FormDataProps = {
+  email: string;
+  password: string;
+}
+
+const signInSchema = yup.object({
+  email: yup.string().required('Informe o e-email.').email('E-mail inválido.'),
+  password: yup.string().required('Informe a senha').min(6, 'A senha deve ter pelo menos 6 dígitos.'),
+})
 
 export function SignIn() {
-
-  const navigation = useNavigation<AuthNavigatorRoutesProps>()
+  const [isLoading, setIsLoading] = useState(false);
+  const { signIn } = useAuth();
+  const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const toast = useToast();
 
   function handleNewAccount() {
-    navigation.navigate("signUp")
+    navigation.navigate("signUp");
   }
+
+  async function handleSignIn({ email, password}: FormDataProps) {
+    try {
+      setIsLoading(true);
+      await signIn(email, password);
+    } catch (err) {
+      const isAppError = err instanceof AppError;
+      const title = isAppError ? err.message : 'Não foi possivel entrar. Tente novamente mais tarde.';
+      
+      setIsLoading(false)
+      
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+      
+    } 
+  }
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signInSchema)
+  })
 
   return(
     <ScrollView contentContainerStyle={{flexGrow: 1}} showsVerticalScrollIndicator={false}>
@@ -39,18 +81,40 @@ export function SignIn() {
             Acesse sua conta
           </Heading>
 
-          <Input 
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
+        <Controller
+          control={control}
+          name="email"
+          render={({ field: { onChange, value }}) => (
+            <Input 
+              placeholder="E-mail"
+              keyboardType="email-address"
+              autoCapitalize="none"
+              onChangeText={onChange}
+              value={value}
+              errorMessage={errors.email?.message}
+            />
+          )}
+        />
 
-          <Input 
-            placeholder="Senha"
-            secureTextEntry
-          />
+        <Controller
+          control={control}
+          name="password"
+          render={({ field: { onChange, value }}) => (
+            <Input 
+              placeholder="Senha"
+              secureTextEntry
+              onChangeText={onChange}
+              value={value}
+              errorMessage={errors.password?.message}
+            />
+          )}
+        />    
 
-          <Button title="Acessar"/>
+          <Button 
+           title="Acessar" 
+           onPress={handleSubmit(handleSignIn)}
+           isLoading={isLoading}
+          />
         </Center>
 
         <Center mt={24}>
