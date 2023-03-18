39e8f25d9c835b9ac32088599b23d2fb8591c4f6
Ericlys Moreira <ericlysm@gmail.com>
2023-03-18 17:02:53 -0300

    refactor: add support to postgres database

--- api/knexfile.js
+++ api/knexfile.js
@@ -1,11 +1,10 @@
 const path = require("path");
+require('dotenv').config()
 
 module.exports = {
   development: {
-    client: "sqlite3",
-    connection: {
-      filename: path.resolve(__dirname, "src", "database", "database.db")
-    },
+    client: process.env.DATABASE_CLIENT ?? "sqlite3",
+    connection:  process.env.DATABASE_CLIENT ? process.env.DATABASE_URL : { filename: path.resolve(__dirname, "src", "database", "database.db")},
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "migrations")
     },
